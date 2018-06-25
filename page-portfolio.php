1a0f95f769e408a70060ef061f18084bc2498be4
HaichuanQD <xinghaichuan8911@gmail.com>
2018-06-25 23:21:30 +0800

    基本写好案例模板

--- page-portfolio.php
+++ page-portfolio.php
@@ -54,30 +54,194 @@
                     </ul>
                     <div id="myTabContent" class="tab-content">
                         <div class="tab-pane fade in active" id="home">
-                            <p>精选案例</p>
+                        <div class="portfolio_cases_container">
+                            <div class="container">
+                                <div class"row">   
+                        <?php $industry = new WP_Query(array( 
+                                'cat' => 16 ));?>
+                            <?php
+                            while ($industry->have_posts()){
+                                $industry->the_post(); ?>
+                           
+                            <div class="col-md-3 col-sm-6">
+                                <div class="cases_container_portfolio" data-aos="fade" data-aos-offset="-999">
+                                <figure class="overflowhidden main_case">   
+                                 <a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {
+	                              the_post_thumbnail('full',  array('class' => 'portfolio_thumbnails'));
+                                 }  ?></a>
+                                 </figure>
+                                 <div class="portfolio_cases_title">
+                                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+                                </div>
+                                </div>
+                                </div>
+                                <?php } wp_reset_postdata();
+                            ?>
+                                </div>
+                               
+                                </div>
+                                </div>
                         </div>
                         <div class="tab-pane fade" id="interior">
-
-                        </div>
-                        <div class="tab-pane fade" id="exterior">
-
-                        </div>
-                        <div class="tab-pane fade" id="industry">
-                            <?php $industry = new WP_Query(array( 
+                        <div class="portfolio_cases_container">
+                            <div class="container">
+                                <div class"row">   
+                        <?php $industry = new WP_Query(array( 
                                 'cat' => 3 ));?>
                             <?php
                             while ($industry->have_posts()){
                                 $industry->the_post(); ?>
-                                <p><?php the_title();?><p>
-                            <?php } wp_reset_postdata();
+                           
+                            <div class="col-md-3 col-sm-6">
+                                <div class="cases_container_portfolio" data-aos="fade" data-aos-offset="-999">
+                                <figure class="overflowhidden main_case">   
+                                 <a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {
+	                              the_post_thumbnail('full',  array('class' => 'portfolio_thumbnails'));
+                                 }  ?></a>
+                                 </figure>
+                                 <div class="portfolio_cases_title">
+                                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+                                </div>
+                                </div>
+                                </div>
+                                <?php } wp_reset_postdata();
                             ?>
+                                </div>
+                               
+                                </div>
+                                </div>
                         </div>
+                        <div class="tab-pane fade" id="exterior">
+                        <div class="portfolio_cases_container">
+                            <div class="container">
+                                <div class"row">   
+                        <?php $industry = new WP_Query(array( 
+                                'cat' => 12 ));?>
+                            <?php
+                            while ($industry->have_posts()){
+                                $industry->the_post(); ?>
+                           
+                            <div class="col-md-3 col-sm-6">
+                                <div class="cases_container_portfolio" data-aos="fade" data-aos-offset="-999">
+                                <figure class="overflowhidden main_case">   
+                                 <a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {
+	                              the_post_thumbnail('full',  array('class' => 'portfolio_thumbnails'));
+                                 }  ?></a>
+                                 </figure>
+                                 <div class="portfolio_cases_title">
+                                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+                                </div>
+                                </div>
+                                </div>
+                                <?php } wp_reset_postdata();
+                            ?>
+                                </div>
+                               
+                                </div>
+                                </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="industry">
+                        <div class="portfolio_cases_container">
+                            <div class="container">
+                                <div class"row">   
+                        <?php $industry = new WP_Query(array( 
+                                'cat' => 13 ));?>
+                            <?php
+                            while ($industry->have_posts()){
+                                $industry->the_post(); ?>
+                           
+                            <div class="col-md-3 col-sm-6">
+                                <div class="cases_container_portfolio" data-aos="fade" data-aos-offset="-999">
+                                <figure class="overflowhidden main_case">   
+                                 <a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {
+	                              the_post_thumbnail('full',  array('class' => 'portfolio_thumbnails'));
+                                 }  ?></a>
+                                 </figure>
+                                 <div class="portfolio_cases_title">
+                                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+                                </div>
+                                </div>
+                                </div>
+                                <?php } wp_reset_postdata();
+                            ?>
+                                </div>
+                               
+                                </div>
+                                </div>
+                        </div>
+                       
                         <div class="tab-pane fade" id="product">
-                            <p>产品案例</p>
+                        <div class="portfolio_cases_container">
+                            <div class="container">
+                                <div class"row">   
+                        <?php $industry = new WP_Query(array( 
+                                'cat' => 14 ));?>
+                            <?php
+                            while ($industry->have_posts()){
+                                $industry->the_post(); ?>
+                           
+                            <div class="col-md-3 col-sm-6">
+                                <div class="cases_container_portfolio" data-aos="fade" data-aos-offset="-999">
+                                <figure class="overflowhidden main_case">   
+                                 <a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {
+	                              the_post_thumbnail('full',  array('class' => 'portfolio_thumbnails'));
+                                 }  ?></a>
+                                 </figure>
+                                 <div class="portfolio_cases_title">
+                                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+                                </div>
+                                </div>
+                                </div>
+                                <?php } wp_reset_postdata();
+                            ?>
+                                </div>
+                               
+                                </div>
+                                </div>
+
+
+                                
+                                
+ 
                         </div>
+
                         <div class="tab-pane fade" id="others_case">
-                            <p>其他案例</p>
+                        <div class="portfolio_cases_container">
+                            <div class="container">
+                                <div class"row">   
+                        <?php $industry = new WP_Query(array( 
+                                'cat' => 17 ));?>
+                            <?php
+                            while ($industry->have_posts()){
+                                $industry->the_post(); ?>
+                           
+                            <div class="col-md-3 col-sm-6">
+                                <div class="cases_container_portfolio" data-aos="fade" data-aos-offset="-999">
+                                <figure class="overflowhidden main_case">   
+                                 <a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {
+	                              the_post_thumbnail('full',  array('class' => 'portfolio_thumbnails'));
+                                 }  ?></a>
+                                 </figure>
+                                 <div class="portfolio_cases_title">
+                                <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+                                </div>
+                                </div>
+                                </div>
+                                <?php } wp_reset_postdata();
+                            ?>
+                                </div>
+                               
+                                </div>
+                                </div>
+
+
+                                
+                                
+ 
                         </div>
+
+
 
                     </div>
                 </div>
