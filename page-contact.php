3438fda904c0e17a6f53331215ee02e3a12303d6
HaichuanQD <xinghaichuan8911@gmail.com>
2018-08-11 09:58:29 +0800

    更改地址为府都大厦

--- page-contact.php
+++ page-contact.php
@@ -26,14 +26,14 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8">
-                <div class="guide-map hidden-xs"><iframe width='560' height='440' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://lbs.amap.com/console/show/map/?1_560_440_120.405713_36.072195&=%E6%B1%87%E6%96%B0%E6%85%A7%E8%89%BA%E6%95%88%E6%9E%9C%E5%9B%BE&=%E9%80%8D%E9%81%A5%E4%B8%89%E8%B7%AF10%E5%8F%B7&=&=15610002502&='></iframe></div>
-                    <div class="guide-map visible-xs-block"><iframe width='300' height='300' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://lbs.amap.com/console/show/map/?0_350_300_120.405919_36.072226&=%E6%B1%87%E6%96%B0%E6%85%A7%E8%89%BA%E6%95%88%E6%9E%9C%E5%9B%BE&=%E9%80%8D%E9%81%A5%E4%B8%89%E8%B7%AF10%E5%8F%B7&=&=15610002502&='></iframe></div>
+                <div class="guide-map hidden-xs"><iframe width='560' height='440' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='http://f.amap.com/7Anx_0E25nT8'></iframe></div>
+                    <div class="guide-map visible-xs-block"><iframe width='300' height='300' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='http://f.amap.com/2K2Yr_0C25nGW'></iframe></div>
             </div>
             <div class="col-lg-4 contact_subtitle">
                 <h3 class="align-left">
                     联系方式：
                 </h3>
-                <p>地址：青岛市市南区逍遥三路10号</p>
+                <p>地址：青岛市市南区南京路8号府都大厦</p>
                 <p>业务QQ：619463273</p>
                 <p>电话：156-1000-2502</p>
                 <p>联系人：邢经理</p>
